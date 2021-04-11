db56d781211321b9e57227a0402dc2885eca289b
Sárdi András <sardi.andras@gmail.com>
2021-04-11 18:27:02 +0200

    Create Penztarca.java

---
+++ Penztarca.java
@@ -0,0 +1,18 @@
+public class Penztarca {
+    private String szin;
+    private String anyag;
+    private double meret;
+    private int penzmennyiseg;
+
+    public Penztarca(String szin, String anyag, double meret, int penzmennyiseg) {
+        this.szin = szin;
+        this.anyag = anyag;
+        this.meret = meret;
+        this.penzmennyiseg = penzmennyiseg;
+    }
+
+    public String toString(){
+        return "A penztarca " + szin + " szinu, " + anyag + " anyagu. A merete: " + meret + ", a benne levo penzmennyiseg: " + penzmennyiseg + ".";
+    }
+
+}
